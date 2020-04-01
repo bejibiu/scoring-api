aa646f72801a27a132f603832312aec44a6176a8
6ejibiu <akimovpnz@live.com>
2020-04-01 17:44:58 +0300

    add decorator retry

--- store.py
+++ store.py
@@ -1,5 +1,26 @@
 import redis
 import logging
+import functools
+
+
+def retry_command(retry_connection=3):
+    def decorator(f):
+        @functools.wraps(f)
+        def reconnector(*args, **kwargs):
+            retry_connection_ = args[0].retry_connection if isinstance(args[0], StorageRedis) else retry_connection
+            try:
+                return f(*args, **kwargs)
+            except (ConnectionError, TimeoutError) as e:
+                while retry_connection_ > 0:
+                    logging.info(f"connection lost. {retry_connection_} attempts left")
+                    try:
+                        return f(*args, **kwargs)
+                    except (ConnectionError, TimeoutError):
+                        retry_connection_ -= 1
+                logging.error("connection failed")
+                raise e
+        return reconnector
+    return decorator
 
 
 class StorageRedis:
@@ -10,37 +31,25 @@ class StorageRedis:
 
     def cache_get(self, key):
         try:
-            result = self.retry_command(self.redis.get, key)
+            return self.get(key)
         except (ConnectionError, TimeoutError):
             return None
-        return result.decode() if result else None
 
+    @retry_command()
     def get(self, key):
-        result = self.retry_command(self.redis.get, key)
+        result = self.redis.get(key)
         return result.decode() if result else None
 
     def cache_set(self, key, score, cached_time):
         try:
-            return self.retry_command(self.redis.set, name=key, value=score, ex=cached_time)
+            return self.set(key, score, cached_time)
         except (ConnectionError, TimeoutError):
             return None
 
-    def set(self, key, value):
-        return self.retry_command(self.redis.set, name=key, value=value)
+    @retry_command()
+    def set(self, key, value, ex=None):
+        return self.redis.set(name=key, value=value, ex=ex)
 
+    @retry_command()
     def remove(self, key):
-        return self.retry_command(self.redis.delete, key)
-
-    def retry_command(self, command, *args, **kwargs):
-        retry_connection = self.retry_connection
-        try:
-            return command(*args, **kwargs)
-        except (ConnectionError, TimeoutError) as e:
-            while retry_connection > 0:
-                logging.info(f"connection lost. {retry_connection} attempts left")
-                try:
-                    return command(*args, **kwargs)
-                except (ConnectionError, TimeoutError):
-                    retry_connection -= 1
-            logging.error("connection failed")
-            raise e
+        return self.redis.delete(key)
